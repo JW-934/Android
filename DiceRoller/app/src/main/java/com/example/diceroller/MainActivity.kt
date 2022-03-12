3cb18d3a4f6d5ce8b9d313549006d2c1c432543c
Jesse <jessew7980@gmail.com>
2022-03-12 12:58:30 -0800

    Update DiceRoller

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 // Allows user to roll a die and view the result.
@@ -14,6 +14,9 @@ class MainActivity : AppCompatActivity() {
         // Non default code
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
+        // Roll when the app starts
+        rollDice()
 
         // First method
         //val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
@@ -27,10 +30,37 @@ class MainActivity : AppCompatActivity() {
         // Creates new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        /* First method
+        when(diceRoll){
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }*/
+
+        // Finds correct image for dice roll
+        val drawableResource = when (diceRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Updates ImageView with correct image ID
+        diceImage.setImageResource(drawableResource)
+
+        // Sets description for accessibility
+        diceImage.contentDescription = diceRoll.toString()
 
         // Updates the screen with roll value
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        //val resultTextView: TextView = findViewById(R.id.textView)
+        //resultTextView.text = diceRoll.toString()
     }
 }
 
